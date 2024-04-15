390ab68a1350dae5400642eacca1c1be7e5fb457
Kusum Lamichhane <kusumlamichhane2015@gmail.com>
2024-04-14 23:43:00 -0400

    Changes in docker files

--- src/project/settings.py
+++ src/project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '#y4g7_4j8&mzf*xcgu$6c*siw^sh2cc7sr_(+5vvfhyfw*5-44'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','127.0.0.1']
+ALLOWED_HOSTS = ['yourdomain.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
